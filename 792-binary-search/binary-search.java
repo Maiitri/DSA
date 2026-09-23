d2cd14638c2e3d60787c1dde000306a022ca02a8
Maiitri <maitrichaudhari076@gmail.com>
2026-09-23 23:00:46 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (30.79%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,20 +1,18 @@
 class Solution {
-    //Maiitri
-
     public int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
 
         while (left <= right) {
-            int mid = left + ((right - left) >> 1);
+            int mid = left + (right - left) / 2;
 
             if (nums[mid] == target) {
                 return mid;
-            }
-
-            if (nums[mid] < target) {
+            } 
+            else if (nums[mid] < target) {
                 left = mid + 1;
-            } else {
+            } 
+            else {
                 right = mid - 1;
             }
         }
